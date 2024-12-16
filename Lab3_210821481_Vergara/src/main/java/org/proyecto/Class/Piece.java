509c4b238c38c7c1459f04238c5ab353e23a8a79
Sofia Vergara <sofia.vergara@usach.cl>
2024-12-15 21:33:59 -0300

    Piece

---
+++ Lab3_210821481_Vergara/src/main/java/org/proyecto/Class/Piece.java
@@ -0,0 +1,22 @@
+package org.proyecto.Class;
+
+public class Piece {
+    private String color;
+
+    // Constructor
+    public Piece(String color) {
+        this.color = color;
+    }
+
+    // Getter
+    public String getColor() {
+        return color;
+    }
+
+    // Setter
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
+
